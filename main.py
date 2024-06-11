8643717c7765c48186b8d039d8a91f21b1825d36
Maid Alisic <114804258+maidalisic@users.noreply.github.com>
2024-06-12 00:02:15 +0200

    main.py

--- main.py
+++ main.py
@@ -7,10 +7,15 @@ import time
 
 # Webdriver-Optionen festlegen
 options = webdriver.ChromeOptions()
-options.binary_location = ChromeDriverManager().install()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--disable-gpu')
+options.add_argument('--remote-debugging-port=9222')
+options.add_argument('--window-size=1920x1080')
 
 # Verwende WebDriverManager zum Installieren von ChromeDriver
-driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
 try:
     # Öffne die Webseite
